42ababa8af09583efac60cf5f8f89dab0c5b4c80
Prabhjot <777prabhjot@gmail.com>
2023-09-22 14:05:54 +0530

    created home page and showed posts

--- app/page.js
+++ app/page.js
@@ -1,9 +1,35 @@
-import Image from "next/image";
+"use client";
+import Card from "@/components/Card";
+import { gql, useQuery } from "@apollo/client";
 
 export default function Home() {
+  const POSTS = gql`
+    query getAllPosts {
+      posts {
+        title
+        content
+        createdAt
+        authorId
+        image
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(POSTS);
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="p-3 border-2 w-fit border-blue-400">hello</div>
+    <div className="flex flex-wrap justify-evenly items-center pt-10">
+      {data?.posts?.map((post) => {
+        return (
+          <div key={post.title}>
+            <Card
+              id={post.id}
+              title={post.title}
+              image={post.image}
+              content={post.content}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
